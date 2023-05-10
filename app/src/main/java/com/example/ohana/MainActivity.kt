ee5079c8d420d29224f8bc33ced3ea0da4689123
AlocasMiller <129418751+AlocasMiller@users.noreply.github.com>
2023-05-11 00:42:17 +0700

    update from igor

--- app/src/main/java/com/example/ohana/MainActivity.kt
+++ app/src/main/java/com/example/ohana/MainActivity.kt
@@ -6,57 +6,46 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectDragGestures
-import androidx.compose.foundation.gestures.rememberTransformableState
-import androidx.compose.foundation.gestures.transformable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DrawerState
 import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDrawerState
-import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.RectangleShape
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalLayoutDirection
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.ohana.ui.theme.BlockHolder
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import kotlin.math.roundToInt
@@ -75,14 +64,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MainMenu() {
     var isRunning by remember { mutableStateOf(false) }
+    val holder = BlockHolder()
 
     Scaffold(
         containerColor = Color(red = 64, green = 61, blue = 57, alpha = 255),
         floatingActionButton = {
             if (!isRunning) {
                 RunButton({ isRunning = !isRunning })
-            }
-            else {
+            } else {
                 StopButton({ isRunning = !isRunning })
             }
         },
@@ -93,7 +82,11 @@ fun MainMenu() {
 
         RightMenu(scope, drawerState)
 
-        Box(modifier = Modifier.padding(contentPadding), contentAlignment = Alignment.BottomEnd) {  }
+        holder.blocks.forEach {
+
+        }
+
+        Box(modifier = Modifier.padding(contentPadding), contentAlignment = Alignment.BottomEnd) { }
     }
 
 }
@@ -115,7 +108,7 @@ fun RunButton(onClick: () -> Unit) {
 @Composable
 fun StopButton(onClick: () -> Unit) {
     FloatingActionButton(
-        onClick = {  onClick()  },
+        onClick = { onClick() },
         containerColor = Color(red = 230, green = 57, blue = 70, alpha = 255),
         shape = CircleShape
     ) {
@@ -142,10 +135,10 @@ fun RightMenu(scope: CoroutineScope, drawerState: DrawerState) {
                     ),
                     modifier = Modifier.width(300.dp)
                 ) {
-                    MenuItem("Controllers", {})
-                    MenuItem("Operators", {})
-                    MenuItem("Variables", {})
-                    MenuItem("Events", {})
+                    MenuItem("Controllers", { CreateBlock() })
+                    MenuItem("Operators", { CreateBlock() })
+                    MenuItem("Variables", { CreateBlock() })
+                    MenuItem("Events", { CreateBlock() })
                 }
             },
             content = {
@@ -161,13 +154,17 @@ fun RightMenu(scope: CoroutineScope, drawerState: DrawerState) {
 }
 
 @Composable
-fun MenuItem(text: String, onClick: () -> Unit) {
+fun MenuItem(text: String, onClick: @Composable () -> Unit) {
+    val flag = remember { mutableStateOf(false) }
     NavigationDrawerItem(
         label = { Text(text, fontSize = 20.sp) },
         selected = false,
-        onClick = { onClick() },
+        onClick = { flag.value = true },
         shape = RectangleShape
     )
+    if (flag.value) {
+        onClick()
+    }
 }
 
 @Composable
