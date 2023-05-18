678bdc019975209e6781032d63832cfa945602f0
Elena Karelina <karelinae03@mail.ru>
2023-05-19 02:33:40 +0700

    block print

--- app/src/main/java/com/example/ohana/MainActivity.kt
+++ app/src/main/java/com/example/ohana/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredWidth
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.layout.wrapContentWidth
@@ -38,8 +39,11 @@ import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
@@ -51,16 +55,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterStart
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.consumePositionChange
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.LayoutDirection
@@ -75,8 +84,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MainMenu()
-            NumberInputBlock()
-            NumberInputBlock()
+
+
 
         }
     }
@@ -107,19 +116,76 @@ fun MainMenu() {
         } else {
             RightMenu(scope, drawerState)
         }
-        Box(modifier = Modifier.padding(contentPadding), contentAlignment = Alignment.BottomEnd) { }
+        Box(modifier = Modifier.padding(contentPadding), contentAlignment = Alignment.BottomEnd) {
+            ValueInputBlock()
+            PrintBlock()
+        }
     }
 }
 
 @Composable
-fun NumberInputBlock() {
-    var value1 by remember { mutableStateOf("") }
-    var value2 by remember { mutableStateOf("") }
+fun PrintBlock() {
+    var value by remember { mutableStateOf("") }
 
+    var minWidth by remember { mutableStateOf(10.dp) }
     var offsetX by remember { mutableStateOf(0f) }
     var offsetY by remember { mutableStateOf(0f) }
     var isDragging by remember { mutableStateOf(false) }
 
+    Box(
+        modifier = Modifier
+            .wrapContentSize()
+            .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
+            .pointerInput(Unit) {
+                detectDragGestures(
+                    onDragStart = { isDragging = true },
+                    onDragEnd = { isDragging = false },
+                    onDrag = { change, dragAmount ->
+                        offsetX += dragAmount.x
+                        offsetY += dragAmount.y
+                        change.consumePositionChange()
+                    }
+                )
+            }
+            .background(Color(0xFF8338EC), shape = RoundedCornerShape(8.dp))
+            .padding(10.dp)
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                text = "Print",
+                modifier = Modifier.padding(horizontal = 4.dp),
+                color = Color.White,
+                fontSize = 20.sp
+            )
+
+            BasicTextField(
+                value = value,
+                onValueChange = { value = it },
+                modifier = Modifier
+                    .background(Color.White, shape = RoundedCornerShape(8.dp))
+                    .padding(vertical = 8.dp, horizontal = 10.dp)
+                    .widthIn(min = minWidth)
+                    .height(IntrinsicSize.Min)
+                    .width(IntrinsicSize.Min)
+                    .wrapContentHeight(),
+                singleLine = true,
+                maxLines = 1,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Text)
+            )
+        }
+    }
+}
+@Composable
+fun ValueInputBlock() {
+    var value1 by remember { mutableStateOf("") }
+    var value2 by remember { mutableStateOf("") }
+
+    var minWidth by remember { mutableStateOf(10.dp) }
+    var offsetX by remember { mutableStateOf(0f) }
+    var offsetY by remember { mutableStateOf(0f) }
+    var isDragging by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -138,33 +204,34 @@ fun NumberInputBlock() {
             }
             .background(Color(0xFF2A9D8F), shape = RoundedCornerShape(8.dp))
             .padding(10.dp)
-            .width(IntrinsicSize.Min)
     ) {
         Row(
             verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier
-                //.fillMaxWidth(0.5f)
-                //.height(50.dp)
-        )  {
-
+        ) {
             BasicTextField(
                 value = value1,
-                onValueChange = { value1 = it },
+                onValueChange = { newValue ->
+                    value1 = newValue.replace(Regex("[^a-z]"), "")
+                },
                 modifier = Modifier
                     .background(Color.White, shape = RoundedCornerShape(8.dp))
                     .padding(vertical = 8.dp, horizontal = 10.dp)
                     .height(IntrinsicSize.Min)
                     .width(IntrinsicSize.Min)
+                    .widthIn(min = minWidth)
                     .wrapContentHeight(),
                 singleLine = true,
                 maxLines = 1,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                keyboardActions = KeyboardActions(onDone = { /* Handle done action if needed */ })
+                keyboardOptions = KeyboardOptions(
+                    keyboardType = KeyboardType.Text,
+                    imeAction = ImeAction.Done
+                ),
+                visualTransformation = VisualTransformation.None
             )
 
 
             Text(
-                text = "+",
+                text = "=",
                 modifier = Modifier.padding(horizontal = 4.dp),
                 color = Color.White,
                 fontSize = 30.sp
@@ -176,18 +243,17 @@ fun NumberInputBlock() {
                 modifier = Modifier
                     .background(Color.White, shape = RoundedCornerShape(8.dp))
                     .padding(vertical = 8.dp, horizontal = 10.dp)
+                    .widthIn(min = minWidth)
                     .height(IntrinsicSize.Min)
                     .width(IntrinsicSize.Min)
                     .wrapContentHeight(),
                 singleLine = true,
                 maxLines = 1,
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                keyboardActions = KeyboardActions(onDone = { /* Handle done action if needed */ })
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
         }
     }
 }
-
 @Composable
 fun RightMenu(scope: CoroutineScope, drawerState: DrawerState) {
     val isControllersDropdownOpen = remember { mutableStateOf(false) }
