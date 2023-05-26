4ae8f5a2e31ecaf0d226d563d326d30012a70ecc
AlocasMiller <129418751+AlocasMiller@users.noreply.github.com>
2023-05-27 00:10:53 +0700

    locked orientation

--- app/src/main/java/com/example/ohana/ui/blocks/Blocks.kt
+++ app/src/main/java/com/example/ohana/ui/blocks/Blocks.kt
@@ -1,13 +1,11 @@
 package com.example.ohana.ui.blocks
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
@@ -25,17 +23,14 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ohana.logic.PrintBlock
 import com.example.ohana.logic.IfBlock
 import com.example.ohana.logic.SetVariableBlock
-import kotlin.math.roundToInt
 
 
 @Composable
