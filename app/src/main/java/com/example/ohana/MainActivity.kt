665e1fee606ae8cccd805f7ebba870474b1fdfcd
AlocasMiller <129418751+AlocasMiller@users.noreply.github.com>
2023-05-26 23:47:57 +0700

    locked orientation

--- app/src/main/java/com/example/ohana/MainActivity.kt
+++ app/src/main/java/com/example/ohana/MainActivity.kt
@@ -1,22 +1,21 @@
 package com.example.ohana
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
@@ -24,10 +23,9 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.unit.IntOffset
+import androidx.compose.ui.unit.dp
 import com.example.ohana.*
 import com.example.ohana.logic.Block
 import com.example.ohana.logic.Interpretator
@@ -36,17 +34,6 @@ import com.example.ohana.logic.Scope
 import com.example.ohana.logic.SetVariableBlock
 import com.example.ohana.ui.blocks.*
 import com.example.ohana.ui.user_interface.*
-
-import android.graphics.Canvas
-import android.graphics.Point
-import android.view.View
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.unit.dp
-import androidx.core.content.res.ResourcesCompat
 import org.burnoutcrew.reorderable.ReorderableItem
 import org.burnoutcrew.reorderable.detectReorderAfterLongPress
 import org.burnoutcrew.reorderable.rememberReorderableLazyListState
@@ -101,6 +88,7 @@ fun MainMenu() {
 
 
 // Отрисовка всех блоков
+@SuppressLint("SuspiciousIndentation")
 @Composable
 fun BlocksDrawer(blocks: MutableList<Block>) {
     val state = rememberReorderableLazyListState(onMove = { from, to ->
